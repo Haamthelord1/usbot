28648aa16691a33b03b5199a6cd2bd5c998aae80
Haamthelord1 <94069564+Haamthelord1@users.noreply.github.com>
2022-07-09 15:20:37 +0530

    Update BuildVars.java

--- src/main/java/se/anyro/userinfobot/BuildVars.java
+++ src/main/java/se/anyro/userinfobot/BuildVars.java
@@ -1,6 +1,6 @@
 package se.anyro.userinfobot;
 
 public class BuildVars {
-    public static final int OWNER = // Your user id here. Send message to @userinfobot to get it.
-    public static final String TOKEN = // Your bot token here. Use @BotFather.
+    public static final int OWNER = "5508479179" // Your user id here. Send message to @userinfobot to get it.
+    public static final String TOKEN = "5410349487:AAFFAsWy-mULKan_9cYtrgFkoozO1IsZrjk" // Your bot token here. Use @BotFather.
 }
